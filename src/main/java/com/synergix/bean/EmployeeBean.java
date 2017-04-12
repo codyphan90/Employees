196ad11c228b353095d34269704d8ecef6823f9c
phanvietanh <phanvietanh90@gmail.com>
2017-04-12 11:57:01 +0700

    add new ,detail xhtml

--- src/main/java/com/synergix/bean/EmployeeBean.java
+++ src/main/java/com/synergix/bean/EmployeeBean.java
@@ -3,10 +3,16 @@ import model.Employee;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.ConfigurableNavigationHandler;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+
+import org.primefaces.event.SelectEvent;
 
 @ManagedBean
 @SessionScoped
@@ -15,7 +21,7 @@ public class EmployeeBean {
     EntityManager entityManager;
 	
 	private List<Employee> employees;
-	
+	private Employee selectedEmployee;
 	@PostConstruct
 	public void init(){
 		String str = "SELECT n FROM Employee n";
@@ -31,22 +37,55 @@ public class EmployeeBean {
 		this.employees = employees;
 	}
 	
-	public void create(){
-		//input code first 
-		
+	
+	public Employee getSelectedEmployee() {
+		return selectedEmployee;
 	}
+
+	public void setSelectedEmployee(Employee selectedEmployee) {
+		this.selectedEmployee = selectedEmployee;
+	}
+	
+	public void onRowSelectNavigate(SelectEvent event) {
+		System.out.println("Navigation executed");
+		ConfigurableNavigationHandler configurableNavigationHandler =
+    			(ConfigurableNavigationHandler)FacesContext.
+    			  getCurrentInstance().getApplication().getNavigationHandler();
+    	
+    	  configurableNavigationHandler.performNavigation("detail.xhtml?faces-redirect=true");
+        
+    }
+	public void redirectNew(SelectEvent event) {
+		System.out.println("Navigation executed");
+		ConfigurableNavigationHandler configurableNavigationHandler =
+    			(ConfigurableNavigationHandler)FacesContext.
+    			  getCurrentInstance().getApplication().getNavigationHandler();
+    	
+    	  configurableNavigationHandler.performNavigation("new.xhtml?faces-redirect=true");
+        
+    }
+	public void create(ActionEvent actionEvent) {
+		//input code first 
+        addMessage("Welcome to Primefaces!!");
+    }
+	public void close(ActionEvent actionEvent) {
+		//back to summary
+    }
 	public void close(){
 		//back to summary
 	}
-	
-	public void submit(){
+	public void submit(ActionEvent actionEvent) {
 		// change status from Draft ('D') --> outstanding ('O')
-		// have confirmation mess
+				// have confirmation mess
+    }
 		
-	}
-	public void abort(){
+	public void abort(ActionEvent actionEvent){
 		//delete then back to summary
 		// have confirmation mess
 	}
+	public void addMessage(String summary) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
 	
 }
